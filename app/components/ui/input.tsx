5058a172e042dbfef41d9d8c43d509854b690bbe
Gerwin Overeem <gerwin@overeem.io>
2025-02-03 15:42:08 +0100

    fix: Add type prop to input

--- app/components/ui/input.tsx
+++ app/components/ui/input.tsx
@@ -2,6 +2,7 @@ export const Input = (props: React.ComponentProps<'input'>) => (
     <input
         // More of these should be configurable, e.g. colors.
         className="w-full bg-white placeholder:text-grey-storm text-sm rounded-sm px-4 py-3 outline-1 outline-transparent transition duration-200 ease focus:outline-afternoon-blue"
+        type={props.type}
         {...props}
     />
 )
